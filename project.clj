75867330b66531b8d18bf8df8eea72b371736490
Larry Jones <mrwizard82d1@gmail.com>
2015-02-04 16:07:43 -0600

    Added lein-cljsbuild to project. Ran rhino-repl.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2760"]]
+  :plugins [[lein-cljsbuild "1.0.4"]]
+  :cljsbuild {:builds []})
